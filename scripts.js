993e052d57ace1480a30cc5c2f424e9a489f41a8
nanireo <61157090+nanireo@users.noreply.github.com>
2024-06-22 00:50:24 +0530

    "web"

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     const slides = document.querySelectorAll('.mySlides');
+//     slides.forEach(slide => {
+//         slide.style.display = 'none';
+//     });
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+//     }
+//     slides[slideIndex - 1].style.display = 'block';
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+// function toggleMenu() {
+//     const navLinks = document.getElementById('navLinks');
+//     const menuIcon = document.querySelector('.menu-icon .bar');
+//     const crossIcon = document.querySelector('.menu-icon .cross-icon');
+
+//     navLinks.classList.toggle('show');
+//     if (navLinks.classList.contains('show')) {
+//         menuIcon.style.display = 'none';
+//         crossIcon.style.display = 'block';
+//     } else {
+//         menuIcon.style.display = 'block';
+//         crossIcon.style.display = 'none';
+//     }
+// }
